32c99704af217acdb815fb190770d7e6fb12cfe7
aimdexter <draimade@gmail.com>
2022-09-16 16:27:22 +0100

    chore: added rfc to allowed commit msg

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,6 +7,7 @@
 // refactor: A code change that neither fixes a bug nor adds a feature
 // style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 // test: Adding missing tests or correcting existing tests
+//rfc : Changing project structure
 
 module.exports = {
     extends: ['@commitlint/config-conventional'],
@@ -33,6 +34,7 @@ module.exports = {
           'build',
           'chore',
           'ci',
+          'rfc',
           'docs',
           'feat',
           'fix',
