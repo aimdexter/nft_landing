f67c5a67bc1601225a0eaa04c58711bef9b06ce6
aimdexter <draimade@gmail.com>
2022-09-16 15:52:53 +0100

    setting husky

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'black': '#0D0D0D',
-      'gray': '#BFBFBF',
-      'red': '#FF0000',
-      'green': '#34D178',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white1': '#ffffff',
+        'black1': '#0D0D0D',
+        'gray1': '#BFBFBF',
+        'red1': '#FF0000',
+        'green1': '#34D178',
+      },
     },
-
   },
   plugins: [],
 }
