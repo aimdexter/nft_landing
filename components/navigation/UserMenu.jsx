74df0ff9d6625f9f2aa69c5ad93adb710ffe71cb
aimdexter <draimade@gmail.com>
2022-09-27 20:19:29 +0100

    feat: nextauth enabled but still need to fix google error

--- components/navigation/UserMenu.jsx
+++ components/navigation/UserMenu.jsx
@@ -1,9 +1,11 @@
+import { signOut, useSession, signIn } from 'next-auth/react';
 import { AiOutlineLogin, AiOutlineLogout } from 'react-icons/ai';
 import { BiUserCircle } from 'react-icons/bi';
 import { RiWechatLine } from 'react-icons/ri';
 import { TbBellRinging } from 'react-icons/tb';
 
 const UserMenu = () => {
+  const { data: session } = useSession();
   return (
     <div className="hidden sticky top-0 bg-bg1 z-40 px-5 border-b-[1px] border-line2 py-4 lg:flex justify-between gap-5 w-full">
       <div className="">
@@ -16,16 +18,25 @@ const UserMenu = () => {
         />
       </div>
       <div className="text-3xl flex flex-row-reverse gap-5 text-gray1">
-        <BiUserCircle />
-        <TbBellRinging />
-        <RiWechatLine />
-        <buton className="flex flex-row-reverse gap-2 items-center">
-          <AiOutlineLogin />
-          <h1 className="text-sm">Login</h1>
-        </buton>
-        <buton className="flex flex-row gap-2 items-center">
+        <div className="flex items-center gap-2 flex-col">
+          <BiUserCircle />
+          <h1 className="text-sm">Signed in as</h1>
+        </div>
+        <div className="flex items-center gap-2 flex-col">
+          <TbBellRinging />
+          <h1 className="text-sm">100</h1>
+        </div>
+        <div className="flex items-center gap-2 flex-col">
+          <RiWechatLine />
+          <h1 className="text-sm">15</h1>
+        </div>
+        <buton onClick={() => signOut()} className="flex flex-col gap-2 items-center">
           <AiOutlineLogout />
           <h1 className="text-sm">Logout</h1>
+        </buton>
+        <buton onClick={() => signIn()} className="flex flex-col gap-2 items-center">
+          <AiOutlineLogin />
+          <h1 className="text-sm">Login</h1>
         </buton>
       </div>
     </div>
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET
+}
+export default NextAuth(authOptions)
